a2f96c1f0ff8dd7eaa35821f396ffb8aaa3553ee
unknown <adnsu.ismail2003@gmail.com>
2024-11-07 01:09:59 +0400

    first commit

---
+++ src/main/java/com/example/springboot/config/RabbitMQConfig.java
@@ -0,0 +1,35 @@
+package com.example.springboot.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${rabbitmq.queue.name}")
+    private String queue;
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+    @Value("${rabbitmq.routing-key.name}")
+    private String routing_key;
+
+    @Bean
+    public Queue queue() {
+        return new Queue(queue);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(exchange);
+    }
+
+    @Bean
+    public Binding binding() {
+        return BindingBuilder
+                .bind(queue())
+                .to(exchange())
+                .with(routing_key);
+    }
+}
